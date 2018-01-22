28a5af76406dc7eae69be991784a218aaa926582
opacut <oliver.pacut@gmail.com>
2018-01-22 16:24:28 +0100

    added directories, modified normal maps

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    public List<Node> nodes = new List<Node>();
+    public bool End;
+    public Text text;
+
+    void Start()
+    {
+        End = false;
+        text.text = "";
+    }
+
+    void Update()
+    {
+        if (!nodes.Exists(y => y.value != 0))
+        {
+            End = true;
+        }
+
+        if (End)
+        {
+            text.text = "You have won!";
+        }
+    }
+}
