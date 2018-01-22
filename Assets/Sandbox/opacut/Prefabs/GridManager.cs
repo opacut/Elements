10004ff005ecbc93e997480b4b427396eeed138a
opacut <oliver.pacut@gmail.com>
2018-01-22 19:43:57 +0100

    moved graphics to materials, deleted .meta files

---
+++ Assets/Sandbox/opacut/Prefabs/GridManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public enum GridSpaceStatus { E, N, B };
+
+    public struct Coordinate
+    {
+        public int x, y;
+        public Coordinate(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+        public bool Compare(int p1, int p2)
+        {
+            return p1 == x && p2 == y;
+        }
+    }
+
+    public struct GridSpace
+    {
+        public Coordinate xy;
+        public GridSpaceStatus status;
+        
+        public GridSpace(Coordinate c, GridSpaceStatus stat)
+        {
+            xy = new Coordinate(c.x,c.y);
+            status = stat;
+        }
+    }
+
+    public Dictionary<Coordinate,GridSpaceStatus> grid { get; set; }
+    public int gridSize;
+
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 1; i >= gridSize; i++)
+        {
+            for (int j = 1; j >= gridSize; j++)
+            {
+                grid[new Coordinate(i, j)] = GridSpaceStatus.E;
+            }
+        }
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    public bool AddBridge(int x, int y)
+    {
+        Coordinate coord = new Coordinate(x, y);
+        bool created = false;
+        foreach(var elem in grid)
+        {
+            if (coord.Compare(elem.Key.x, elem.Key.y))
+            {
+                if (elem.Value != GridSpaceStatus.E)
+                {
+                    break;
+                }
+                else
+                {
+                    grid[coord] = GridSpaceStatus.E;
+                    created = true;
+                    break;
+                }
+            }
+        }
+        return created;
+    }
+}
---
+++ Assets/Sandbox/opacut/Prefabs/WinGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinGame : MonoBehaviour
+{
+    public List<Node> nodes = new List<Node>();
+    public bool EndGame;
+    public Text text;
+
+    void Start ()
+    {
+        EndGame = false;
+        text.text = "";
+    }
+	
+	void Update ()
+    {
+        if (!nodes.Exists(y => y.value!=0))
+        {
+            EndGame = true;
+        }
+        
+        if(EndGame)
+        {
+            text.text = "You have won!";
+        }
+	}
+}
