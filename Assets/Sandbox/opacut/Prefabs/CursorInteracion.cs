4e7cb3ecde7c4e1179d2c9a95dadb83f56dc9bf7
opacut <oliver.pacut@gmail.com>
2018-01-23 16:00:48 +0100

    tidied up a bit, simplified textures and colliders, added falloff colliders

---
+++ Assets/Sandbox/opacut/Prefabs/CursorInteracion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorInteracion : MonoBehaviour
+{
+    public int rayLength = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var hit = new RaycastHit();
+        var fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, hit.distance, rayLength))
+        {
+            if (hit.collider.gameObject.tag == "bridge")
+            {
+                hit.collider.gameObject.SendMessage(methodName: "OnMouseOver");
+                //guiShow = true;
+                //if(Input.GetKe)
+            }
+
+        }
+    }
+}
