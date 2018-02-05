694da5b2dd8ea1c5663c589c2b6e025211010ae0
Kristýna Valíková <valikri13@gmail.com>
2018-02-05 03:52:46 +0100

    Add menu

---
+++ GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+	public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+	}
+
